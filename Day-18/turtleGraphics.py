fbc765639871356248d14e576eec18037b2087c4
Arthur Kateeba <arthurkateeba@gmail.com>
2025-12-05 00:09:56 +0300

    add todo task no.4 to implement random walk

--- Day-18/turtleGraphics.py
+++ Day-18/turtleGraphics.py
@@ -26,14 +26,48 @@ random_color = generate_color()
 # Todo: 2 Draw a dashed line
 
 # position turtle at the start
-turtle.penup()
-turtle.goto(-200,200)
+#turtle.penup()
+#turtle.goto(-200,200)
+#turtle.clear()
+turtle.pensize(10)
 
-for _ in range(10):
-    turtle.pendown()
-    turtle.fd(20)
-    turtle.penup()
-    turtle.fd(20)
+# for _ in range(10):
+#     turtle.pendown()
+#     turtle.fd(20)
+#     turtle.penup()
+#     turtle.fd(20)
+
+# Todo: 3 Draw different shapes
+
+sides_and_colors = {
+    3: 'red',
+    4: 'green',
+    6: 'blue',
+    7: 'yellow',
+    8: 'gray',
+    9: 'pink',
+    10: 'violet',
+}
+
+# for sides, color in sides_and_colors.items():
+#     turtle.color(color)
+#     for _ in range(sides):
+#         turtle.fd(100)
+#         turtle.rt(360 / sides)
+
+# Todo: 4 Random walk
+
+import random
+angle_direction = [turtle.rt, turtle.lt]
+
+for _ in range(200):
+    clr = generate_color()
+    turtle.pencolor(clr)
+    turn = random.choice(angle_direction)
+    turtle.fd(15)
+    turn(90)
+    turtle.fd(15)
+
 
 
 # turtle.clear()
